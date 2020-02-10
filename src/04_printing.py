69963b6539250c5b9057142b8b81e2d3f7c424e6
chudnofskyATD <mpereannor@gmail.com>
2020-02-10 22:28:23 +0000

    gaps filled

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,10 +11,10 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+print("x is %d, y is %.2f, z is %s" %(x, round(y,2), z))
 
 # Use the 'format' string method to print the same thing
-print('{x} and {y} and {z}'.format(10, 2.24552, 'I like turtles'))
+print (' x is {0}, y is {1}, z is {2}'.format(x, round(y,2), z))
 
 # Finally, print the same thing using an f-string
 print(f'{x} {y} {z}')
